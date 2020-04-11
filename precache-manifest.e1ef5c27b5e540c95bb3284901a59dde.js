95f0cce4345ccd5286d72f408952e340d5a8a5de
Jay Barbhaiya <jaybarb1990@gmail.com>
2020-04-11 21:51:35 +0200

    Updates

---
+++ precache-manifest.e1ef5c27b5e540c95bb3284901a59dde.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b98d66aa866e69f3df8078b3377f58e",
+    "url": "/coronavirus-tracker/index.html"
+  },
+  {
+    "revision": "d50cf2ae2c0445994f18",
+    "url": "/coronavirus-tracker/static/css/main.634addd9.chunk.css"
+  },
+  {
+    "revision": "77da11c7f184ac2770f6",
+    "url": "/coronavirus-tracker/static/js/2.28b5afee.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/coronavirus-tracker/static/js/2.28b5afee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d50cf2ae2c0445994f18",
+    "url": "/coronavirus-tracker/static/js/main.8cabdaa9.chunk.js"
+  },
+  {
+    "revision": "35c858d73af1965f2e45",
+    "url": "/coronavirus-tracker/static/js/runtime-main.39f88733.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/coronavirus-tracker/static/media/image.d7265326.png"
+  }
+]);
