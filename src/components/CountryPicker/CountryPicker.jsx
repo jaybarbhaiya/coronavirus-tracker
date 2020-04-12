f56a22b998a2adb66e50e6b9f680afd4a4427e7c
Jay Barbhaiya <jaybarb1990@gmail.com>
2020-04-12 12:14:22 +0200

    Adapting title and adding label for country picker

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { NativeSelect, FormControl } from '@material-ui/core';
+import { NativeSelect, FormControl, Typography } from '@material-ui/core';
 
 import styles from './CountryPicker.module.css';
 import { fetchCountries } from '../../api';
@@ -17,6 +17,7 @@ const CountryPicker = ({ handleCountryChange }) => {
 
 	return (
 		<FormControl className={styles.FormControl}>
+			<Typography variant="body2">Choose country: </Typography>
 			<NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
 				<option value="">Global</option>
 				{fetchedCountries.map((country, i) => <option key={i} value={country}>{country}</option>)}
